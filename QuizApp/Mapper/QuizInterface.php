efee98e5e34e20a6c65da762305587674052266e
robboyland <boylandrobert@gmail.com>
2014-11-30 12:31:09 +0000

    add Mapper QuizInterface

---
+++ QuizApp/Mapper/QuizInterface.php
@@ -0,0 +1,15 @@
+<?php namespace QuizApp\Mapper;
+
+interface QuizInterface
+{
+    /**
+     * @return \QuizApp\Entity\Quiz[]
+     */
+    public function findAll();
+
+    /**
+     * @param int $i
+     * @return \QuizApp\Entity\Quiz
+     */
+    public function find($i);
+}
