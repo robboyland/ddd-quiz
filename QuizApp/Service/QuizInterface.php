b768a3fed22b750840dcf43bc744c99ff1852d1c
robboyland <boylandrobert@gmail.com>
2014-11-30 12:29:02 +0000

    add Service QuizInterface

---
+++ QuizApp/Service/QuizInterface.php
@@ -0,0 +1,21 @@
+<?php namespace QuizApp\Service;
+
+interface QuizInterface
+{
+    /** @return \QuizApp\Entity\Quiz[] */
+    public function showAllQuizes();
+
+    public function startQuiz($quizOrId);
+
+    /** @return \QuizApp\Entity\Question */
+    public function getQuestion();
+
+    /** @return bool */
+    public function checkSolution($id);
+
+    /** @return bool */
+    public function isOver();
+
+    /** @return Quiz\Result */
+    public function getResult();
+}
