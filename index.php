bac0b43e4ca485a5bd78842942fe50d0bc7bb93b
robboyland <boylandrobert@gmail.com>
2014-11-30 13:00:37 +0000

    add index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'vendor/autoload.php';
+
+session_start();
+
+$service = new QuizApp\Service\Quiz(
+    new \QuizApp\Mapper\HardCoded()
+);
+
+$app = new \Slim\Slim();
+$app->config(array('templates.path' => './views'));
+$app->get('/', function () use ($service, $app) {
+    $app->render('choose-quiz.phtml', array(
+        'quizes' => $service->showAllQuizes(),
+    ));
+});
+$app->get('/choose-quiz/:id', function ($id) use ($service, $app) {
+    $service->startQuiz($id);
+    $app->redirect('/solve-question');
+});
+$app->get('/solve-question', function () use ($service, $app) {
+    $app->render('solve-question.phtml', array(
+        'question' => $service->getQuestion(),
+    ));
+});
+$app->post('/check-answer', function () use ($service, $app) {
+    $isCorrect = $service->checkSolution($app->request->post('id'));
+    if (!$service->isOver()) {
+        $app->redirect('/solve-question');
+    } else {
+        $app->redirect('/end');
+    }
+});
+
+$app->get('/end', function () use ($service, $app) {
+    $app->render('end.phtml', array(
+        'result' => $service->getResult(),
+    ));
+});
+
+$app->run();
