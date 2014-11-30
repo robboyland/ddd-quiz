3e1659983f01ee19a2c9ecc166e8704113478677
robboyland <boylandrobert@gmail.com>
2014-11-30 13:00:20 +0000

    refactor psr-4 autoloading path

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit54b72052b9620486bc042b1b618e3fdb::getLoader();
+return ComposerAutoloaderInit8b8b187537a41fc07103b53a7101fd24::getLoader();
